f851c9f583c5e0f8d987da2db84223084fabb415
JalalJaberi <j_jaberi@yhoo.com>
2018-06-08 03:38:10 +0430

    fixed bug

--- EGController.php
+++ EGController.php
@@ -69,6 +69,6 @@ class EGController extends \yii\web\Controller
 	{
 		if (in_array('elephantsGroup\stat\traits\StatTrait', class_uses($this)))
 			$this->log();
-		return !parent::beforeAction($action);
+		return parent::beforeAction($action);
 	}
 }
